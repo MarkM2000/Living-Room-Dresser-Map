c1b3e33200dca4e2c9a0e1e695e6af2802985f4c
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-03-27 15:24:58 -0400

    Create sideboard.js

---
+++ location_js/dressing/sideboard.js
@@ -0,0 +1,523 @@
+var sideboard = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "DE6A",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -75.23528,
+            38.51722
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "GA10A",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -81.92861,
+            31.93639
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "GA10B",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -82.13889,
+            32.28028
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "GA11N",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -82.01972,
+            32.16028
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY25A",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.96167,
+            43.61833
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "GA16A",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -81.87695,
+            33.02195
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "GA24A",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -83.64584,
+            32.81667
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "GA9A",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -82.01722,
+            31.48611
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "MD18A",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -77.04472,
+            39.09222
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "MD21A",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -76.58472,
+            38.54028
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "MD23A",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -77.21722,
+            38.45472
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "MD23B",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -77.22833,
+            38.45945
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "MD7A",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -75.335,
+            38.35278
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "MD8A",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -75.65139,
+            38.28056
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NC52A",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -79.73111,
+            35.21528
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC5A",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -80.20834,
+            33.70694
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC5B",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -80.22083,
+            33.68889
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC17A",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -81.53528,
+            33.08055
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC17C",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -81.10973,
+            32.87083
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC19D!",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -80.67944,
+            32.43333
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC24D!",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -80.34167,
+            33.90833
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC29C",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -81.58639,
+            33.51639
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC29C",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -81.58639,
+            33.51639
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC32B",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -81.35,
+            34.16583
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC34A",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -81.29305,
+            34.76833
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC36B",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -81.34111,
+            34.95083
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC42A",
+          "Dresser": "sideboard"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -82.45834,
+            34.875
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "GA076",
+          "Dresser": "sideboard [sic] "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -84.9497,
+            34.2389
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "GA159",
+          "Dresser": "sideboard [sic] "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -81.9285,
+            31.9365
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "TN292",
+          "Dresser": "sideboard [sic] "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -86.5156,
+            36.5782
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "LA667",
+          "Dresser": "sideboard [sic] "
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -91.7528,
+            29.981
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "GA1A",
+          "Dresser": "sideboards"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -83.02556,
+            34.67778
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC10B!",
+          "Dresser": "sideboards"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -79.99167,
+            33.24445
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC10B!",
+          "Dresser": "sideboards"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -79.99167,
+            33.24445
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC12A",
+          "Dresser": "sideboards"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -79.93056,
+            32.76667
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC12A",
+          "Dresser": "sideboards"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -79.93056,
+            32.76667
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC44A",
+          "Dresser": "sideboards"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -82.64722,
+            35.00139
+          ]
+        }
+      }
+    ]
+  }
