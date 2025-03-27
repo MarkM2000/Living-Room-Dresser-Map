c70ba6af776c80cc15be3b6102547275ae1b0286
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-03-27 15:28:22 -0400

    Create best_room.js

---
+++ location_js/living_room/best_room.js
@@ -0,0 +1,215 @@
+var best_room = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY5E",
+          "Response": "best room"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -73.95944,
+            40.66667
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NY63D!",
+          "Response": "best room"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -78.71528,
+            42.15778
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "MD12A",
+          "Response": "best room"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -76.82306,
+            39.53167
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "SC39B!",
+          "Response": "best room"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -81.62083,
+            34.27917
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "MS557",
+          "Response": "best room, parlor"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -88.814,
+            33.4659
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "KY15",
+          "Response": "living room, best room, parlor"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -85.2527,
+            37.5698
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "MI11A",
+          "Response": "living room, front room, best room"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -85.4541,
+            44.1248
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "KY24C",
+          "Response": "living room, parlor, best room"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -87.8353,
+            36.865
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "KY8A",
+          "Response": "parlor, best room, living room"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -84.9547,
+            38.5423
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NH118",
+          "Response": "parlor, sitting room, living room, front room, best room"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -72.1478,
+            43.3011
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "CT142",
+          "Response": "parlor, sitting room, parlor room, best room"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -72.6743,
+            41.7627
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "WI15B",
+          "Response": "parlor, the best room un the house"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -88.1668,
+            43.3239
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NH75",
+          "Response": "setting room, best room, parlor, living room"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -70.8733,
+            42.8936
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "NH181",
+          "Response": "setting room, parlor, best room"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -72.2781,
+            42.9336
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "Informant": "GA149",
+          "Response": "the best room, family room, living room"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            -83.7963,
+            32.0914
+          ]
+        }
+      }
+    ]
+  }
