0d5a09ece2ea1a85c7f1748b1ec4a5938b2a99ed
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-04-02 14:45:44 -0400

    add chest variable

--- js/variables.js
+++ js/variables.js
@@ -69,6 +69,40 @@ var chest_of_drawers = L.geoJson(chest_of_drawers, {
     }
 })
 
+var chest = L.geoJson(chest, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#FFFFFF',
+            weight: 1,
+            fillColor: '#5a5247',
+            fillOpacity: .8,
+            radius: 5
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+					<b>${props.Informant}</b>
+					<br>Dresser type: ${props.Dresser}<br>
+				`
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#5a5247'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#5a5247'
+            });
+        });
+    }
+})
 // Living room
 var best_room = L.geoJson(best_room, {
     pointToLayer: function (feature, latlng) {
