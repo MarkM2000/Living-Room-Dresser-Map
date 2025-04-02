a20fa8e6c954e15e340d5897e80a4aa95a34fc99
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-04-02 15:06:25 -0400

    adding setting room variable

--- js/variables.js
+++ js/variables.js
@@ -278,3 +278,38 @@ var parlor = L.geoJson(parlor, {
         });
     }
 })
+
+var setting_room = L.geoJson(setting_room, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#FFFFFF',
+            weight: 1,
+            fillColor: '#5a5247',
+            fillOpacity: .8,
+            radius: 5
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+					<b>${props.Informant}</b>
+					<br>Dresser type: ${props.Response}<br>
+				`
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#5a5247'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#5a5247'
+            });
+        });
+    }
+})
