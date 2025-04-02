ff3ce094293a82d7cabb51ca2c10211d0570c4a6
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-04-02 15:14:07 -0400

    add sitting room variable

--- js/variables.js
+++ js/variables.js
@@ -313,3 +313,38 @@ var setting_room = L.geoJson(setting_room, {
         });
     }
 })
+
+var sitting_room = L.geoJson(sitting_room, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#FFFFFF',
+            weight: 1,
+            fillColor: '#5a5247',
+            fillOpacity: .8,
+            radius: 5
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+					<b>${props.Informant}</b>
+					<br>Dresser type: ${props.Response}<br>
+				`
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#5a5247'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#5a5247'
+            });
+        });
+    }
+})
