a604e6ba6e80cb16ac9c004a36d0a7373fda7817
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-04-02 15:04:38 -0400

    adding parlor variable

--- js/variables.js
+++ js/variables.js
@@ -243,3 +243,38 @@ var living_room = L.geoJson(living_room, {
         });
     }
 })
+
+var parlor = L.geoJson(parlor, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#FFFFFF',
+            weight: 1,
+            fillColor: '#5a5247',
+            fillOpacity: .8,
+            radius: 5
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+					<b>${props.Informant}</b>
+					<br>Dresser type: ${props.Response}<br>
+				`
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#5a5247'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#5a5247'
+            });
+        });
+    }
+})
