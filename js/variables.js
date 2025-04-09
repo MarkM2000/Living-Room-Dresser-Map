6a32ab403bbc1da8bc0165311d21dea51f4a839c
MarkM2000 <142824282+MarkM2000@users.noreply.github.com>
2025-04-09 14:40:46 -0400

    adding highboy variable

--- js/variables.js
+++ js/variables.js
@@ -138,6 +138,41 @@ var dresser = L.geoJson(dresser, {
         });
     }
 })
+
+var highboy = L.geoJson(highboy, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, {
+            color: '#FFFFFF',
+            weight: 1,
+            fillColor: '#5a5247',
+            fillOpacity: .8,
+            radius: 5
+        });
+    },
+    onEachFeature: function (feature, layer) {
+        const props = feature.properties
+        const popup = `
+					<b>${props.Informant}</b>
+					<br>Dresser type: ${props.Dresser}<br>
+				`
+        layer.bindTooltip(popup, {
+            className: 'tool-informant'
+        });
+
+        layer.on('mouseover', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#5a5247'
+            });
+        });
+        layer.on('mouseout', function () {
+            // code goes in here
+            layer.setStyle({
+                fillColor: '#5a5247'
+            });
+        });
+    }
+})
 // Living room
 var best_room = L.geoJson(best_room, {
     pointToLayer: function (feature, latlng) {
